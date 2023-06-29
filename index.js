5bc64ed9eebcdbc89c670795b77d2c0adc4d87ab
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-06-28 22:46:57 -0300

    beta 1.0

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ buttonClick.addEventListener("click", function(){
 if (switchButton === true) {
     document.querySelector("html").classList.add('light');
     switchButton = false;
+    document.querySelector('.profile img').src = "assets/avatar.png"
     console.log(switchButton)
 } else {
     document.querySelector("html").classList.remove('light');
+    document.querySelector('.profile img').src = "assets/avatar.png"
     switchButton = true
     console.log(switchButton)
 }
